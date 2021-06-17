f3bd25e30693349641d270fdd0c533b59954503b
iklam <ioi.lam@oracle.com>
2021-06-17 15:18:32 -0700

    added a benchmark to see if we can change ResourceHashtable::SIZE to a runtime-configuration parameter

---
+++ bench/resourceHash/resourceHashNew.hpp
@@ -0,0 +1,108 @@
+/*
+ * Copyright (c) 2012, 2020, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ *
+ */
+
+template<
+    typename K, typename V,
+    unsigned (*HASH)  (K const&)           = primitive_hash<K>,
+    bool     (*EQUALS)(K const&, K const&) = primitive_equals<K>>
+class ResourceHashtableNew {
+ private:
+
+  class Node {
+   public:
+    unsigned _hash;
+    K _key;
+    V _value;
+    Node* _next;
+
+    Node(unsigned hash, K const& key, V const& value) :
+        _hash(hash), _key(key), _value(value), _next(NULL) {}
+
+    // Create a node with a default-constructed value.
+    Node(unsigned hash, K const& key) :
+        _hash(hash), _key(key), _value(), _next(NULL) {}
+
+  };
+  unsigned int _size;
+  Node** _table;
+
+  // Returns a pointer to where the node where the value would reside if
+  // it's in the table.
+  Node** lookup_node(unsigned hash, K const& key) {
+    unsigned index = hash % _size;
+    Node** ptr = &_table[index];
+    while (*ptr != NULL) {
+      Node* node = *ptr;
+      if (node->_hash == hash && EQUALS(key, node->_key)) {
+        break;
+      }
+      ptr = &(node->_next);
+    }
+    return ptr;
+  }
+
+  Node const** lookup_node(unsigned hash, K const& key) const {
+    return const_cast<Node const**>(
+        const_cast<ResourceHashtableNew*>(this)->lookup_node(hash, key));
+  }
+
+ public:
+  ResourceHashtableNew(unsigned size) {
+    _size = size;
+    _table = new Node*[size];
+    memset(_table, 0, _size * sizeof(Node*));
+  }
+
+  bool contains(K const& key) const {
+    return get(key) != NULL;
+  }
+
+  V* get(K const& key) const {
+    unsigned hv = HASH(key);
+    Node const** ptr = lookup_node(hv, key);
+    if (*ptr != NULL) {
+      return const_cast<V*>(&(*ptr)->_value);
+    } else {
+      return NULL;
+    }
+  }
+
+ /**
+  * Inserts or replaces a value in the table.
+  * @return: true:  if a new item is added
+  *          false: if the item already existed and the value is updated
+  */
+  bool put(K const& key, V const& value) {
+    unsigned hv = HASH(key);
+    Node** ptr = lookup_node(hv, key);
+    if (*ptr != NULL) {
+      (*ptr)->_value = value;
+      return false;
+    } else {
+      *ptr = new Node(hv, key, value);
+      return true;
+    }
+  }
+};
+
---
+++ bench/resourceHash/test.cpp
@@ -0,0 +1,120 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef unsigned int           uintptr_t;
+typedef long long              intx;
+typedef unsigned long long     uintx;
+#define UINT64_FORMAT          "%llu"
+
+template<typename K> unsigned primitive_hash(const K& k) {
+  unsigned hash = (unsigned)((uintptr_t)k);
+  return hash ^ (hash >> 3); // just in case we're dealing with aligned ptrs
+}
+
+template<typename K> bool primitive_equals(const K& k0, const K& k1) {
+  return k0 == k1;
+}
+
+#include "resourceHash.hpp"
+#include "resourceHashNew.hpp"
+
+inline uintx test_modulo_const(uintx num) {
+  return num % 6661;
+}
+
+inline uintx test_modulo(uintx num, uint *div) {
+  return num % (*div);
+}
+
+volatile uint num = 0;
+volatile uint keys[] = {0, 111, 222, 333, 444, 555, 666, 777, 888, 999};
+
+uintx test_0(intx loops, uintx v) {
+  for (intx i = 0; i < loops; i++) {
+    v += test_modulo_const(v);
+  }
+
+  return v;
+}
+
+uintx test_1(intx loops, uintx v) {
+  uint div = num + 6661;
+
+  for (intx i = 0; i < loops; i++) {
+    v += test_modulo(v, &div);
+  }
+
+  return v;
+}
+
+ResourceHashtable<uintx, uintx, 6661> table_2;
+
+uintx test_2(intx loops, uint* keys, int num_keys) {
+  uintx result = num;
+
+  for (intx i = 0; i < loops; i++) {
+    for (intx n = 0; n < num_keys; n++) {
+      uintx* p = table_2.get(keys[n]);
+      result += *p;
+    }
+  }
+
+  return result;
+}
+
+ResourceHashtableNew<uintx, uintx> table_3(6661);
+
+uintx test_3(intx loops, uint* keys, int num_keys) {
+  uintx result = num;
+
+  for (intx i = 0; i < loops; i++) {
+    for (intx n = 0; n < num_keys; n++) {
+      uintx* p = table_3.get(keys[n]);
+      result += *p;
+    }
+  }
+
+  return result;
+}
+
+int test(int argc, char** argv) {
+  if (argc < 3) {
+    printf("Usage: %s <which> <loops>\n", argv[0]);
+    exit(1);
+  }
+  intx which = atoll(argv[1]);
+  intx loops = atoll(argv[2]);
+
+  uintx v = (uintx)loops;
+
+  switch (which) {
+  case 0:
+    v = test_0(loops, v);
+    break;
+  case 1:
+    v = test_1(loops, v);
+    break;
+  case 2:
+    for (int n = 0; n < 10; n++) {
+      table_2.put(keys[n], 1);
+    }
+    v = test_2(loops, (uint*)keys, 10);
+    break;
+  case 3:
+    for (int n = 0; n < 10; n++) {
+      table_3.put(keys[n], 1);
+    }
+    v = test_3(loops, (uint*)keys, 10);
+    break;
+  }
+
+  printf("which = %d v = " UINT64_FORMAT "\n", which, v);
+
+  return (int)(v + v >> 32); // make sure the compiler doesn't elide the whole thing.
+}
+
+int main(int argc, char** argv) {
+  test(argc, argv);
+  return 0;
+}
