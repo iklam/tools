6ca3595b0b622aa20e137150fa7d9738a685b066
iklam <ioi.lam@oracle.com>
2022-04-04 15:05:31 -0700

    Improved jbs_status.tc

--- scripts/misc/jbs_status.tcl
+++ scripts/misc/jbs_status.tcl
@@ -3,9 +3,7 @@
 # Currently, this script does only this (which I do like a million times a day)
 # - get a JBS bug ID from the X clipboard
 # - print out the full URL for this JBS issue
-#
-# TODO:
-# Also print out if the bug has been resolved, etc.
+# - Also print out if the bug has been resolved, etc.
 
 set N {[0-9]}
 set pat $N$N$N$N$N$N$N
@@ -22,5 +20,37 @@ puts ""
 set fd [open "|xclip -i" w+]
 puts -nonewline $fd $url
 close $fd
+
+flush stdout
+
+if {[catch {
+    set data [exec wget -O - -q $url]
+    if {[regexp {<title>([^<]+)</title>} $data dummy  title]} {
+        regsub { - Java Bug System} $title "" title
+        regsub {^[^\]]+\] } $title "" title
+        puts "    Title:              $title"
+    }
+
+    regsub {<h4 class="toggle-title">Description</h4>.*} $data "" data
+    regsub -all {<[^>]+>} $data "" data
+    regsub "\n *Description.*" $data "" data
+    regsub .*Type: $data Type: data
+    #regsub -all "\[\r\n\t \]+" $data " " data
+
+    set sep ""
+    foreach n [split $data \n] {
+        if {[regexp : $n]} {
+            puts -nonewline "$sep    [format %-20s [string trim $n]]"
+            set sep \n
+        } else {
+            foreach w $n {
+                puts -nonewline "$w "
+            }
+        }
+    }
+    puts ""
+} err]} {
+    puts "Cannot get url $err"
+}
 
 
